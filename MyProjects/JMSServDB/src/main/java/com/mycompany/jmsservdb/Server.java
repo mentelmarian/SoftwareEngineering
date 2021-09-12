466e6c2b96b6ef0955ba3d971d2249fa328ef787
Marian Leonard <mentelmarian@gmail.com>
2021-09-12 18:34:08 +0200

    Add files via upload

---
+++ MyProjects/JMSServDB/src/main/java/com/mycompany/jmsservdb/Server.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jmsservdb;
+
+/**
+ *
+ * @author studente
+ */
+public class Server {
+      public static void main(String args[]) throws Exception {
+        FlightProductor productor = new FlightProductor();
+        productor.start();      
+    }
+}
